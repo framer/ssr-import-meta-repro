60bfca2801c20a7a14565f370792771c42bc198a
Vladimir Guguiev <wizardzloy@gmail.com>
2021-09-10 15:57:14 +0200

    nextjs issues demo

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.experiments = {
+      buildHttp: true
+    };
+
+    // config.module.rules.push(
+    //     {
+    //       resource: /^https:\/\/framer(usercontent)?\.com\//,
+    //       resolve: {
+    //         fullySpecified: false
+    //       }
+    //     },
+    // )
+
+    return config
+  },
+  experimental: {
+      esmExternals: "loose"
+  },
 }
